97320249954fc7a1e38d23880ad31d02d025a2f1
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-08-09 22:33:13 +0200

    simple mobile version of beta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, 'overflow-x-hidden')}>{children}</body>
+      <body className={cn(inter.className)}>{children}</body>
     </html>
   );
 }
