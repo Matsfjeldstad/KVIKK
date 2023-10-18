ffa7308577ad6fdafb9a95d2cf34e09a6c4ce5d5
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-10-18 14:02:40 +0200

    update model to gpt-3.5-turbo-istruct, and added toast error

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,5 +1,6 @@
 import { Configuration, OpenAIApi } from 'openai-edge';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { toast } from 'sonner';
 
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
@@ -17,10 +18,11 @@ export async function POST(req: Request) {
   const ResponsePrompt = `Write a response to the following email. Please ensure that the response is written in the same language as provided here: ${language}, unless otherwise specified.The tone of the response should be ${mood}. This is a(n) ${type} email. The sender of the email is ${name}, so you are making the email in the name of ${name}. If necessary, please use the following additional context to inform your response: ${context} Email provided: '${prompt}'. format it like an email. length should be ${length}.`;
   const NewEmailPromt = `Genrate an email. Please ensure that the email is written in the same language as provided here: ${language}, unless otherwise specified. The tone of the email should be ${mood}. This is a(n) ${type} email. The sender of the email is ${name}, so you are making the email in the name of ${name}. If necessary, please use the following additional context to inform your email: ${prompt}'. format it like an email. length should be ${length}.`;
   // Ask OpenAI for a streaming completion given the prompt
+  
   const response = await openai.createCompletion({
-    model: 'text-davinci-003',
+    model: 'gpt-3.5-turbo-instruct',
     stream: true,
-    max_tokens: 300,
+    max_tokens: 500,
     temperature: 1,
     prompt: isResponse ? ResponsePrompt : NewEmailPromt,
   });
