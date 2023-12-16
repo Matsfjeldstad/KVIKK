8e032d152728642f671c0faa7273d48c441239b9
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-12-16 02:09:34 +0100

    adds new user state to dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import GeneratorPage from "../generator/page";
+import { NewUserModal } from "./new-user";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/lib/auth";
 
-type Props = {};
+type Props = {
+    searchParams: { [new_user: string]: string | string[] | undefined };
+};
 
-export default function page({}: Props) {
-    return <GeneratorPage />;
+async function serverUserData() {
+    const userData = await getServerSession(authConfig);
+
+    if (!userData) {
+        return null;
+    }
+
+    return userData.user;
+}
+
+export default async function page({ searchParams }: Props) {
+    const data = await serverUserData();
+    const isNewUser = searchParams.new_user === "true";
+    return (
+        <div>
+            <GeneratorPage />
+            <NewUserModal isNewUser={isNewUser} data={{id:data?.id!, email: data?.email!, name: data?.name!}} />
+        </div>
+    );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -41,11 +41,25 @@ export const authConfig: NextAuthOptions = {
     ],
     pages: {
         verifyRequest: "/verify-request",
-        newUser: "/new-user",
+        newUser: '/dashboard?new_user=true',
         signIn: "/signin",
     },
     callbacks: {
-        session({ session, user }) {
+        async signIn({ user, account, profile, email, credentials }) {
+            const isAllowedToSignIn = true;
+            if (isAllowedToSignIn) {
+                return true;
+            } else {
+                // Return false to display a default error message
+                return false;
+                // Or you can return a URL to redirect to:
+                // return '/unauthorized'
+            }
+        },
+        async redirect({ url, baseUrl }) {
+            return baseUrl;
+        },
+        async session({ session, user }) {
             if (session.user) {
                 session.user.id = user.id;
             }
